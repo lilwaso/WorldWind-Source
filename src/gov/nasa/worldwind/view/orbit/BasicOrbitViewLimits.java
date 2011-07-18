5ecdb193a32cd4c99473933212627c42e0349815
Sean <Seankli123@gmail.com>
2011-07-18 13:48:45 -0400

    Max Zoom Fixed

--- src/gov/nasa/worldwind/view/orbit/BasicOrbitViewLimits.java
+++ src/gov/nasa/worldwind/view/orbit/BasicOrbitViewLimits.java
@@ -30,7 +30,7 @@ public class BasicOrbitViewLimits extends BasicViewPropertyLimits implements Orb
         this.minPitch = Angle.ZERO;
         this.maxPitch = Angle.POS90;
         this.minZoom = 0;
-        this.maxZoom = Double.MAX_VALUE;
+        this.maxZoom = 1000000000;
     }
 
     public Sector getCenterLocationLimits()
